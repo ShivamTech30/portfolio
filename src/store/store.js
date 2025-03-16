7f4340667ca2da77664c3d9c8d2c75576062c95f
shivam <shivamsharma@externlabs.com>
2025-03-16 15:30:10 +0530

    Updates

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import contactReducer from './contactSlice';
-
-export const store = configureStore({
-  reducer: {
-    contact: contactReducer,
-  },
-});
