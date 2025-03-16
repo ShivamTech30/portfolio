c0974eb9c167c34e925c8f177c60f3de9adf6119
shivam <shivamsharma@externlabs.com>
2025-03-16 14:47:47 +0530

    inital name change in package

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"https://ShivamTech30.github.io/",
   resolve: {
     alias: {
       '@': '/src',
