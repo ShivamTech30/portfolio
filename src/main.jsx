7fa6ab66e25874732724cfef1e48146e8c610d32
shivam <shivamsharma@externlabs.com>
2025-04-06 17:01:08 +0530

    add has routing and add one libabry

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import App from './App';
@@ -9,9 +9,9 @@ import './index.css';
 createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   // </React.StrictMode>
 );
